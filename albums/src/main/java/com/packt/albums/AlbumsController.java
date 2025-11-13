636233fe592f07d38adf72d1c89406b2a5b45689
joaoscioli <joaoscioli@outlook.com>
2025-11-13 14:35:15 -0300

    feat (01-rest-api-recipes) finally module albums with FeignClient

---
+++ albums/src/main/java/com/packt/albums/AlbumsController.java
@@ -0,0 +1,24 @@
+package com.packt.albums;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/albums")
+public class AlbumsController {
+    private final FootballClient footballClient;
+    public AlbumsController(FootballClient footballClient) {
+        this.footballClient = footballClient;
+    }
+
+    @GetMapping("/players")
+    public List<Player> getPlayers() {
+        return footballClient.getPlayers();
+    }
+
+
+}
---
+++ albums/src/main/java/com/packt/albums/FootballClient.java
@@ -0,0 +1,13 @@
+package com.packt.albums;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@FeignClient(name = "football", url = "http://localhost:8080")
+public interface FootballClient {
+    @RequestMapping(method = RequestMethod.GET, value = "/players")
+    List<Player> getPlayers();
+}
