58f48367a5b5161baeab8ddf43c5c054c6438514
joaoscioli <joaoscioli@outlook.com>
2025-11-17 19:35:16 -0300

    feat (01-rest-api-recipes) management files from chapter 01

---
+++ fooballobs/src/main/java/com/packt/fooballobs/FooballobsApplication.java
@@ -0,0 +1,13 @@
+package com.packt.fooballobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooballobsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FooballobsApplication.class, args);
+    }
+
+}
---
+++ fooballobs/src/test/java/com/packt/fooballobs/FooballobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packt.fooballobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooballobsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
