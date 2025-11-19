e74a1d775e24d44844966035e3fa011a0b04dc7d
joaoscioli <joaoscioli@outlook.com>
2025-11-19 20:43:02 -0300

    feat (02-database-and-jpa) update class PlayersController

--- fooballclient/src/main/java/com/packt/fooballclient/controller/PlayersController.java
+++ fooballclient/src/main/java/com/packt/fooballclient/controller/PlayersController.java
@@ -1,22 +1,16 @@
-package com.packt.footballclient.controller;
-
-import java.util.List;
-import java.util.stream.Collectors;
+package com.packt.fooballclient.controller;
 
 import com.packt.fooballclient.domain.PlayerRanking;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-
-import io.micrometer.tracing.BaggageInScope;
-import io.micrometer.tracing.Tracer;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/players")
